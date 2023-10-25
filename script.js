d88192a2a32ce38f793c051c4b256e3e63c2cb1c
Roger Pérez <rperezb@campus.eug.es>
2023-10-25 18:08:02 +0200

    incluimos confirmshaming

--- script.js
+++ script.js
@@ -10,9 +10,17 @@ function showLogin() {
 }
 
 const miDiv = document.getElementById('cookie-banner');
+const confirmShaming = document.getElementById('confirmshaming-banner');
 const enlaceActivar = document.getElementById('enlaceActivar');
+const enlaceConfirmShaming = document.getElementById('enlaceConfirmshaming');
 
 enlaceActivar.addEventListener('click', function (event) {
   event.preventDefault(); // Evita que el enlace redireccione a una nueva página
   miDiv.style.display = 'none';
+  confirmShaming.style.display='flex';
+});
+
+enlaceConfirmShaming.addEventListener('click', function (event) {
+  event.preventDefault(); // Evita que el enlace redireccione a una nueva página
+  confirmShaming.style.display='none';
 });
